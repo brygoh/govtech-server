7801bf7c6876caaf4b4da333c452117ecbe97314
Goh Hong Xiang, Bryan <72204360+brygoh@users.noreply.github.com>
2022-09-21 00:11:46 +0800

    removed route

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express')
 
 const app = express()
 const port = 5000
+
+// const champion = require('./routes/champion')
 const urlShortener = require('./routes/url_shortener')
 
 app.use(cors())
 app.use(express.json({ extended: false }));
 
 app.use('/url', urlShortener);
+// app.use('/champion', champion);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
